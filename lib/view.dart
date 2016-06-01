23f0554d6597ae41d1f79ad56a26eaf5a36c28a1
Don <don.j.olmstead@gmail.com>
2016-05-31 23:41:14 -0700

    Add view library

---
+++ lib/view.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/view/codec_class_view.dart';
+export 'src/view/converter_class_view.dart';
+export 'src/view/converter_function_view.dart';
+export 'src/view/converter_library_view.dart';
