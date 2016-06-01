0dfc798792f558094dcb6a7a749cbffaf99ec482
Don <don.j.olmstead@gmail.com>
2016-05-31 23:41:01 -0700

    Add codegen library

---
+++ lib/codegen.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/codegen/codec_class_view.dart';
+export 'src/codegen/converter_class_view.dart';
+export 'src/codegen/converter_function_view.dart';
