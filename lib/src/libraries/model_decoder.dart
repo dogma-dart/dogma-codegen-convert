19be9acc8ce2ef215c05d81bd9436c033ec73db1
Don <don.j.olmstead@gmail.com>
2016-05-31 23:31:11 -0700

    Add model decoder metadata

---
+++ lib/src/libraries/model_decoder.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_analyzer/metadata.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// The name of the model decoder class.
+const String modelDecoderClassName = 'ModelDecoder';
+
+/// The name of the create method.
+///
+/// This is defined on ModelDecoder.
+const String createMethodName = 'create';
+
+/// A generic model decoder type.
+///
+/// The type arguments are not specified which will allow equality matching.
+final genericModelDecoderType = new TypeMetadata(modelDecoderClassName);
+
+/// Creates type metadata for a ModelDecoder using the [model]'s type.
+TypeMetadata modelDecoderType(TypeMetadata model) =>
+    new TypeMetadata(modelDecoderClassName, arguments: [model]);
