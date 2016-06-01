f4e9b87bcffe216acc7b7e9e347f4c5196096e8c
Don <don.j.olmstead@gmail.com>
2016-05-31 23:40:06 -0700

    Add codec view

---
+++ lib/src/view/codec_class_view.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_codegen/view.dart';
+import 'package:dogma_source_analyzer/metadata.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+class CodecClassView extends MetadataView<ClassMetadata> {
+  CodecClassView(ClassMetadata metadata)
+      : super(metadata);
+}
