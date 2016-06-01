46a080d67983016d00118b2e0f40ad15ceac4055
Don <don.j.olmstead@gmail.com>
2016-05-31 23:31:19 -0700

    Add model encoder metadata

---
+++ lib/src/libraries/model_encoder.dart
@@ -0,0 +1,26 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_analyzer/metadata.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// The name of the model encoder class.
+const String modelEncoderClassName = 'ModelEncoder';
+
+/// A generic model encoder type.
+///
+/// The type arguments are not specified which will allow equality matching.
+final genericModelEncoderType = new TypeMetadata(modelEncoderClassName);
+
+/// Creates type metadata for a ModelEncoder using the [model]'s type.
+TypeMetadata modelEncoderType(TypeMetadata model) =>
+    new TypeMetadata(modelEncoderClassName, arguments: [model]);
