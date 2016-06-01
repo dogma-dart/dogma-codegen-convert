b65d4ab9012267f5f118aa0e9acae48a7bab6ccd
Don <don.j.olmstead@gmail.com>
2016-05-31 23:40:53 -0700

    Add codegen for functions

---
+++ lib/src/codegen/converter_function_view.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_codegen/codegen.dart';
+
+import '../../view.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Generates source code for the converter function [view] into the [buffer].
+void generateFunction(ConverterFunctionView view, StringBuffer buffer) {
+}
