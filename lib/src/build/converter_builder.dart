7e1739b5634b4f650142036584fb58d887b2ca14
Don <don.j.olmstead@gmail.com>
2016-06-16 23:19:32 -0700

    Update converter builder

--- lib/src/build/converter_builder.dart
+++ lib/src/build/converter_builder.dart
@@ -12,6 +12,7 @@ import 'package:dogma_codegen/runner.dart';
 import 'package:dogma_codegen_model/build.dart';
 import 'package:dogma_source_analyzer/analyzer.dart';
 
+import 'converter_target_config.dart';
 import 'converter_codegen_step.dart';
 import 'converter_view_generation_step.dart';
 
@@ -19,15 +20,34 @@ import 'converter_view_generation_step.dart';
 // Library contents
 //---------------------------------------------------------------------
 
+/// A [SourceBuilder] that generates code to convert from Map data to Plain
+/// Old Dart Objects (PODOs).
 @RegisterBuilder('convert')
-class ConverterBuilder extends SourceBuilder
+class ConverterBuilder extends SourceBuilder<ConverterTargetConfig>
                           with AnalyzerMetadataStep,
                                ModelViewStep,
                                ConverterViewGenerationStep,
                                ConverterCodegenStep {
-  ConverterBuilder(String package, String libraryOutput)
-      : super(package, libraryOutput, null);
+  //---------------------------------------------------------------------
+  // Constructor
+  //---------------------------------------------------------------------
+
+  /// Creates an instance of [ConverterBuilder] with the given [config].
+  ConverterBuilder(BuilderConfig<ConverterTargetConfig> config)
+      : super(config);
+
+  //---------------------------------------------------------------------
+  // AnalyzerMetadataStep
+  //---------------------------------------------------------------------
 
   @override
   final List<AnalyzeAnnotation> annotationCreators = <AnalyzeAnnotation>[];
+
+  @override
+  String filename(String value) {
+    var split = value.split('.');
+    split[0] = '${split[0]}_convert';
+
+    return split.join('.');
+  }
 }
